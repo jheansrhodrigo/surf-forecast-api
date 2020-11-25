fdac228fc2f9ff3e65dc66796745185a33dc09d3
Jean Silva <jheanrds@gmail.com>
2020-11-25 01:24:11 -0300

    Add error cases

---
+++ src/services/forecast.ts
@@ -0,0 +1,91 @@
+import { StormGlass, ForecastPoint } from '@src/clients/stormGlass';
+import { time } from 'console';
+import { InternalError } from '@src/util/errors/internal-error';
+
+export enum BeachPosition {
+  south = 'S',
+  east = 'E',
+  west = 'W',
+  north = 'N',
+}
+
+export interface Beach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export class ForecastProcessingInternalError extends InternalError {
+  constructor(message: string) {
+    super(`Unexprected error during the forecast processing: ${message}`);
+  }
+}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<TimeForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+
+    try {
+      for (const beach of beaches) {
+        const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+        const enrichedBeachData = this.enrichedBeachData(points, beach);
+
+        pointsWithCorrectSources.push(...enrichedBeachData);
+      }
+
+      return this.mapForecastByTime(pointsWithCorrectSources);
+    } catch (error) {
+      throw new ForecastProcessingInternalError(error.message);
+    }
+  }
+
+  private enrichedBeachData(
+    points: ForecastPoint[],
+    beach: Beach
+  ): BeachForecast[] {
+    return points.map((forecast) => ({
+      ...{
+        lat: beach.lat,
+        lng: beach.lng,
+        name: beach.name,
+        position: beach.position,
+        rating: 1,
+      },
+      ...forecast,
+    }));
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find(
+        (forecast) => forecast.time === point.time
+      );
+
+      if (timePoint) {
+        timePoint.forecast.push();
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+
+    return forecastByTime;
+  }
+}
