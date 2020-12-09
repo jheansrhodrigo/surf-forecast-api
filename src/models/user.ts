c5ec201ef606ab3ce463a34ba1d512707e339470
Jean Silva <jheanrds@gmail.com>
2020-12-09 01:03:46 -0300

    Add hash with bcrypt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Model } from 'mongoose';
+import AuthService from '../services/auth';
 
 export interface User {
   _id?: string;
@@ -28,12 +29,15 @@ const schema = new mongoose.Schema(
       transform: (_, ret): void => {
         ret.id = ret._id;
         delete ret._id;
-        delete ret._v;
+        delete ret.__v;
       },
     },
   }
 );
 
+/**
+ * Validates the email and throws a validation error, otherwise it will throw a 500
+ */
 schema.path('email').validate(
   async (email: string) => {
     const emailCount = await mongoose.models.User.countDocuments({ email });
@@ -42,5 +46,18 @@ schema.path('email').validate(
   'already exists in the database.',
   CUSTOM_VALIDATION.DUPLICATED
 );
+
+schema.pre<UserModel>('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (error) {
+    console.error(`Error hashing the password for the user ${this.name}`);
+  }
+});
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
